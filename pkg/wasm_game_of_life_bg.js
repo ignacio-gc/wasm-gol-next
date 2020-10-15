30833c3c96bcebf3fb12b071cf6d3c565aae8db6
Ignacio Caamaño <ignacio167@gmail.com>
2020-10-15 10:29:05 -0300

    add pkg folder

---
+++ pkg/wasm_game_of_life_bg.js
@@ -0,0 +1,133 @@
+import * as wasm from './wasm_game_of_life_bg.wasm';
+
+const lTextDecoder = typeof TextDecoder === 'undefined' ? (0, module.require)('util').TextDecoder : TextDecoder;
+
+let cachedTextDecoder = new lTextDecoder('utf-8', { ignoreBOM: true, fatal: true });
+
+cachedTextDecoder.decode();
+
+let cachegetUint8Memory0 = null;
+function getUint8Memory0() {
+    if (cachegetUint8Memory0 === null || cachegetUint8Memory0.buffer !== wasm.memory.buffer) {
+        cachegetUint8Memory0 = new Uint8Array(wasm.memory.buffer);
+    }
+    return cachegetUint8Memory0;
+}
+
+function getStringFromWasm0(ptr, len) {
+    return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
+}
+
+let cachegetUint32Memory0 = null;
+function getUint32Memory0() {
+    if (cachegetUint32Memory0 === null || cachegetUint32Memory0.buffer !== wasm.memory.buffer) {
+        cachegetUint32Memory0 = new Uint32Array(wasm.memory.buffer);
+    }
+    return cachegetUint32Memory0;
+}
+
+let WASM_VECTOR_LEN = 0;
+
+function passArray32ToWasm0(arg, malloc) {
+    const ptr = malloc(arg.length * 4);
+    getUint32Memory0().set(arg, ptr / 4);
+    WASM_VECTOR_LEN = arg.length;
+    return ptr;
+}
+
+let cachegetInt32Memory0 = null;
+function getInt32Memory0() {
+    if (cachegetInt32Memory0 === null || cachegetInt32Memory0.buffer !== wasm.memory.buffer) {
+        cachegetInt32Memory0 = new Int32Array(wasm.memory.buffer);
+    }
+    return cachegetInt32Memory0;
+}
+/**
+*/
+export const Cell = Object.freeze({ Dead:0,"0":"Dead",Alive:1,"1":"Alive", });
+/**
+*/
+export class Universe {
+
+    static __wrap(ptr) {
+        const obj = Object.create(Universe.prototype);
+        obj.ptr = ptr;
+
+        return obj;
+    }
+
+    free() {
+        const ptr = this.ptr;
+        this.ptr = 0;
+
+        wasm.__wbg_universe_free(ptr);
+    }
+    /**
+    */
+    tick() {
+        wasm.universe_tick(this.ptr);
+    }
+    /**
+    * @param {number} width
+    * @param {number} height
+    * @param {Uint32Array} pattern
+    * @returns {Universe}
+    */
+    static new_pattern(width, height, pattern) {
+        var ptr0 = passArray32ToWasm0(pattern, wasm.__wbindgen_malloc);
+        var len0 = WASM_VECTOR_LEN;
+        var ret = wasm.universe_new_pattern(width, height, ptr0, len0);
+        return Universe.__wrap(ret);
+    }
+    /**
+    * @param {number} width
+    * @param {number} height
+    * @returns {Universe}
+    */
+    static new(width, height) {
+        var ret = wasm.universe_new(width, height);
+        return Universe.__wrap(ret);
+    }
+    /**
+    * @returns {string}
+    */
+    render() {
+        try {
+            const retptr = wasm.__wbindgen_export_1.value - 16;
+            wasm.__wbindgen_export_1.value = retptr;
+            wasm.universe_render(retptr, this.ptr);
+            var r0 = getInt32Memory0()[retptr / 4 + 0];
+            var r1 = getInt32Memory0()[retptr / 4 + 1];
+            return getStringFromWasm0(r0, r1);
+        } finally {
+            wasm.__wbindgen_export_1.value += 16;
+            wasm.__wbindgen_free(r0, r1);
+        }
+    }
+    /**
+    * @returns {number}
+    */
+    width() {
+        var ret = wasm.universe_width(this.ptr);
+        return ret >>> 0;
+    }
+    /**
+    * @returns {number}
+    */
+    height() {
+        var ret = wasm.universe_height(this.ptr);
+        return ret >>> 0;
+    }
+    /**
+    * @returns {number}
+    */
+    cells() {
+        var ret = wasm.universe_cells(this.ptr);
+        return ret;
+    }
+}
+
+export const __wbindgen_throw = function(arg0, arg1) {
+    throw new Error(getStringFromWasm0(arg0, arg1));
+};
+
