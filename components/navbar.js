c11cc8881b826fb90a236149cd674ac64e61a169
Ignacio Caamaño <ignacio167@gmail.com>
2020-10-17 14:54:21 -0300

    cambios mobile navbar

--- components/navbar.js
+++ components/navbar.js
@@ -2,21 +2,28 @@ import styles from './navbar.module.css'
 import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { useState } from 'react'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faQuestion } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
+  const [toogleState, setState] = useState(false)
+
+  const toggle = () => setState(prevState => !prevState)
 
   return (
-    <nav className={styles.navbar}>
-      <ul className={styles.menu}>
+    <nav>
+      <ul className={`${styles.menu} ${toogleState ? styles.show : styles.hide}`}>
+        <button className="btn" onClick={toggle}><span><FontAwesomeIcon icon={faBars}  size={'2x'} /></span></button>
         <li>
-          <Link href="/acerca"><a href="">Acerca</a></Link>
+          <Link href="/acerca"><a href=""><span><FontAwesomeIcon icon={faQuestion} style={{ color: 'white' }} /></span></a></Link>
         </li>
         <li>
           <a href="https://github.com/ignacio-gc/wasm-gol-next">
-            <span className="font"><FontAwesomeIcon icon={faGithub} style={{ color: 'white' }} /></span> Github</a>
+            <span className="font"><FontAwesomeIcon icon={faGithub} style={{ color: 'white' }} /></span></a>
         </li>
       </ul>
-    </nav >
+    </nav>
   )
 }
 
