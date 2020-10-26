daab6f0f72d35920122c81fb2a6a11d3abfe31b7
Ignacio Caamaño <ignacio167@gmail.com>
2020-10-26 11:13:33 -0300

    with GA1

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,21 @@
 import '../styles/global.css'
 import 'prismjs/themes/prism-tomorrow.css';
-
-export function reportWebVitals(metric) {
-  const body = JSON.stringify(metric)
-  const url = 'https://example.com/analytics'
-
-  // Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
-  if (navigator.sendBeacon) {
-    navigator.sendBeacon(url, body)
-  } else {
-    fetch(url, { body, method: 'POST', keepalive: true })
-  }
-  console.log(metric)
-}
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
       <Component {...pageProps} />
