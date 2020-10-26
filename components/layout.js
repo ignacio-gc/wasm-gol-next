37cc9d7ea99c0b210958f8ecfa8a2dbd7c7e160d
Ignacio Caamaño <ignacio167@gmail.com>
2020-10-26 09:53:32 -0300

    analytics en layout

--- components/layout.js
+++ components/layout.js
@@ -1,8 +1,24 @@
 import Navbar from '../components/navbar'
+import Head from 'next/head'
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-MYQ1L1WNL2"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html:
+              `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-MYQ1L1WNL2');
+          `,
+          }}
+        />
+      </Head>
       <Navbar />
       <main>{children}</main>
     </>
--- pages/_document.js
+++ pages/_document.js
@@ -10,19 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-MYQ1L1WNL2"></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html:
-                `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-MYQ1L1WNL2');
-          `,
-            }}
-          />
+        
         </Head>
         <body>
           <Main />
