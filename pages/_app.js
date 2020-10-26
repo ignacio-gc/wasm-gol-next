02bae0775d8abf0479fd8d4539111ef70898a170
Ignacio Caamaño <ignacio167@gmail.com>
2020-10-26 09:38:42 -0300

    analytics en _document.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,9 @@
 import '../styles/global.css'
 import 'prismjs/themes/prism-tomorrow.css';
-import Head from 'next/head'
-
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-MYQ1L1WNL2"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html:
-              `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-MYQ1L1WNL2');
-          `,
-          }}
-        />
-      </Head>
       <Component {...pageProps} />
     </>
   )
---
+++ pages/_document.js
@@ -0,0 +1,36 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-MYQ1L1WNL2"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html:
+                `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-MYQ1L1WNL2');
+          `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
