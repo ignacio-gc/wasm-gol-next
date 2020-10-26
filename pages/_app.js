26e55435d7bf75df8b3ec5b8806a33d1350e6d0d
Ignacio Caamaño <ignacio167@gmail.com>
2020-10-26 10:33:02 -0300

    más GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,19 @@
 import '../styles/global.css'
 import 'prismjs/themes/prism-tomorrow.css';
 
+export function reportWebVitals(metric) {
+  const body = JSON.stringify(metric)
+  const url = 'https://example.com/analytics'
+
+  // Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
+  if (navigator.sendBeacon) {
+    navigator.sendBeacon(url, body)
+  } else {
+    fetch(url, { body, method: 'POST', keepalive: true })
+  }
+  console.log(metric)
+}
+
 export default function App({ Component, pageProps }) {
   return (
     <>
