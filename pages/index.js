4733d0b099bec7825509498553589427a55fc210
Ignacio Caamaño <ignacio167@gmail.com>
2020-10-29 00:06:11 -0300

    index title

--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,15 @@ import Layout from '../components/layout'
 
 const Page = ({ router: { query } }) => {
   return (
-    <>
-      <Layout>
-        <div className={styles.header}>
-          <h1>El Juego de la vida</h1>
-          <h2>Rust 🦀 + Wasm 🕸️ + Next.js</h2>
-        </div>
-        <div className={styles.gol}>
-          <GameOfLife width={64} height={64} />
-        </div>
-      </Layout>
-    </>
+    <Layout>
+      <div className={styles.header}>
+        <h1>El Juego de la vida</h1>
+        <h2>Rust 🦀 + Wasm 🕸️ + Next.js</h2>
+      </div>
+      <div className={styles.gol}>
+        <GameOfLife width={64} height={64} />
+      </div>
+    </Layout>
   )
 }
 
